d6c5d42949fec7bc6532c2dd5f48d0d0968f212f
TexTrue <65154269+TexBlock@users.noreply.github.com>
2025-06-29 23:50:55 +0800

    sync with `sakura-ryoko/servux` 1.21.3-0.4.5-sakura.3, update ForgifiedFabricAPI (ThinkingStudio fork)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
 ## Change
-- sync with `sakura-ryoko/servux` 1.21.3-0.4.4
-- foxifiednetworking -> forgifiedfabricapi ThinkingStudio fork
+- sync with `sakura-ryoko/servux` 1.21.3-0.4.5-sakura.3
+- update ForgifiedFabricAPI (ThinkingStudio fork) version (Included FFAPI, don't have to download FFAPI)
--- src/main/java/fi/dy/masa/servux/Reference.java
+++ src/main/java/fi/dy/masa/servux/Reference.java
@@ -9,10 +9,10 @@ import org.thinkingstudio.sevuxforged.loader.FoxifiedLoader;
 public class Reference
 {
     public static final String MOD_ID = "servux";
-    public static final String MOD_NAME = "ServuxForged";
+    public static final String MOD_NAME = "ServuX";
     public static final String MOD_VERSION = StringUtils.getModVersionString(MOD_ID);
     public static final String MC_VERSION = MinecraftVersion.CURRENT.getName();
-    public static final String MOD_TYPE = "neoforge";
+    public static final String MOD_TYPE = "fabric";
     public static final String MOD_STRING = MOD_ID + "-" + MOD_TYPE + "-" + MC_VERSION + "-" + MOD_VERSION;
     public static final boolean DEV_DEBUG = false;
 
